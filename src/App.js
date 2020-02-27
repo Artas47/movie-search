6325c5a74c6a57b613b1c808dd3e4fca44cbe4ae
artas <artasss47@gmail.com>
2020-02-27 13:56:41 +0100

    moved reducer and initial state to its own file

--- src/App.js
+++ src/App.js
@@ -10,77 +10,8 @@ import MovieItemDetails from "./components/movie-item-details/movie-item-details
 import { StateProvider } from "./context/state";
 
 function App() {
-  const INITIAL_STATE = {
-    movies: [],
-    currentMovie: null,
-    isLoading: false,
-    searchTerm: "",
-    favMovies: JSON.parse(localStorage.getItem("favMovies")) || [],
-    showSidebar: localStorage.getItem("showSidebar") === "true" ? true : false
-  };
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case "SEARCH_MOVIES_START":
-        return {
-          ...state,
-          isLoading: true
-        };
-      case "SEARCH_MOVIES_SUCCESS":
-        return {
-          ...state,
-          movies: action.movies,
-          isLoading: false
-        };
-      case "FETCH_MOVIE":
-        return {
-          ...state,
-          currentMovie: action.movie
-        };
-      case "MOVIE_UNMOUNT":
-        return {
-          ...state,
-          currentMovie: null
-        };
-      case "TOGGLE_SPINNER":
-        return {
-          ...state,
-          isLoading: !state.isLoading
-        };
-      case "SEARCH_TERM_CHANGE":
-        return {
-          ...state,
-          searchTerm: action.searchTerm
-        };
-      case "ADD_FAV_MOVIE":
-        return {
-          ...state,
-          favMovies: [...state.favMovies, action.movie]
-        };
-      case "REMOVE_FAV_MOVIE":
-        return {
-          ...state,
-          favMovies: state.favMovies.filter(
-            item => item.imdbID !== action.movie.imdbID
-          )
-        };
-      case "ADD_FAV_MOVIES_LS":
-        return {
-          ...state,
-          favMovies: action.moviesLs
-        };
-      case "TOGGLE_SIDEBAR":
-        return {
-          ...state,
-          showSidebar: action.showSidebar === "true" ? true : false
-        };
-      default:
-        return state;
-    }
-  };
-
   return (
-    <StateProvider initialState={INITIAL_STATE} reducer={reducer}>
+    <StateProvider>
       <Styled.App>
         <GlobalStyles />
         <Header />
--- src/context/state.js
+++ src/context/state.js
@@ -1,7 +1,16 @@
 import React, { createContext, useContext, useReducer } from "react";
+import { reducer } from "./reducer";
+const INITIAL_STATE = {
+  movies: [],
+  currentMovie: null,
+  isLoading: false,
+  searchTerm: "",
+  favMovies: JSON.parse(localStorage.getItem("favMovies")) || [],
+  showSidebar: localStorage.getItem("showSidebar") === "true" ? true : false
+};
 export const StateContext = createContext();
-export const StateProvider = ({ reducer, initialState, children }) => (
-  <StateContext.Provider value={useReducer(reducer, initialState)}>
+export const StateProvider = ({ children }) => (
+  <StateContext.Provider value={useReducer(reducer, INITIAL_STATE)}>
     {children}
   </StateContext.Provider>
 );
